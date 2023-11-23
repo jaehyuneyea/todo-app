2e6d618e9a06343ec89f5633a16530b1d4148b53
Jae Hyune Yea <jaehyune.yea@gmail.com>
2023-11-23 15:28:37 -0800

    Added firebase to project using angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"todo-list-test-app-b7446","appId":"1:679799246674:web:1aabbd5a7f21207f0d348b","storageBucket":"todo-list-test-app-b7446.appspot.com","apiKey":"AIzaSyA5DBqX3qTzxgcM_s_elS1XzHV1ybn6Ri4","authDomain":"todo-list-test-app-b7446.firebaseapp.com","messagingSenderId":"679799246674"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
